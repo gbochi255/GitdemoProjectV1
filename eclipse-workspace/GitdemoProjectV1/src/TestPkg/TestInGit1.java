ccac3616ac1b71ce252ad0b2953a98af8669f872
gbochi255 <gbochi255@hotmail.com>
2023-03-21 17:35:55 +0000

    this is first commit

---
+++ eclipse-workspace/GitdemoProjectV1/src/TestPkg/TestInGit1.java
@@ -0,0 +1,15 @@
+package TestPkg;
+
+public class TestInGit1 {
+
+	public static void main(String[] args) {
+		String hungry = "yes";
+		if(hungry=="yes")
+			System.out.println("eat something......");
+		else {
+			System.out.println("do work.......");
+		}
+
+	}
+
+}
---
+++ eclipse-workspace/GitdemoProjectV1/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author gboch
+ *
+ */
+module GitdemoProjectV1 {
+}
